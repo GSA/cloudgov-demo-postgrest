0fc7bc9138d0e15344c21f30c56f12bdaea9a63f
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-03-05 20:53:11 -0800

    Consolidate everything into start.sh
    Drop use of an explicit anon role until later

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+set -e
+
 # Make sure we can find any installed python scripts
 export PATH=$PATH:/home/vcap/deps/0/python/bin/
 
 # If there's a csv/import.sh file, run it. Otherwise inspect, create tables, and import data from csv/*.csv
-[ -x ./data/import.sh ] && ./data/import.sh || csvsql --db $DATABASE_URL --db-schema $DB_SCHEMA --insert data/*.csv
+[ -x ./data/import.sh ] && ./data/import.sh || csvsql --overwrite --db $DATABASE_URL --db-schema $DB_SCHEMA --insert data/*.csv
 
-# Set up the anonymous and authenticator roles 
-./setup-roles.py
+# TODO: Set up explicit anonymous and authenticator roles 
+#   ./setup-roles.py
+# For now, let's just use the role that the platform gave us
+[[ $DATABASE_URL =~ (.*)'://'(.*):(.*)@(.*):(.*?)/(.*) ]] && export DB_ANON_ROLE=${BASH_REMATCH[2]}
 
-# DB_URI=${DATABASE_URL} exec ./env-to-config ./postgrest postgrest.conf
+export DB_URI=${DATABASE_URL}
+exec ./env-to-config ./postgrest postgrest.conf
