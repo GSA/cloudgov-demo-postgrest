4c005e40ae3a284c542e95f34ad56a943d8ee2ee
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-03-06 17:55:10 -0800

    Change default to a new schema, and do our best to make sure it exists
    The Python script is always run now.

--- data/initdb.py
+++ data/initdb.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
-# An easy way to make DB queries without the `psql` client available.
-# Just connects to the $DATABASE_URL and runs the content of any passed arguments as SQL
+# Does initial setup on the database to make it ready for postgREST.
+# Expects to read DATABASE_URL, DB_SCHEMA, and DB_ANON_ROLE from the environment.
+# 
+# This script also provides a way to make DB queries without the `psql` client available.
+# It runs the content of any passed arguments as files full of SQL queries.
 
 import os
 import psycopg2
@@ -15,6 +18,24 @@ except:
 
 cur = conn.cursor()
 
+# Do the database setup
+try:
+    # Create the schema
+    cur.execute('CREATE SCHEMA IF NOT EXISTS ' + os.environ['DB_SCHEMA'])
+
+    # Create a read-only role to be used for any unauthenticated user
+    cur.execute('CREATE ROLE ' + os.environ['DB_ANON_ROLE'])
+    cur.execute('GRANT USAGE ON SCHEMA ' + os.environ['DB_SCHEMA'] + ' TO ' + os.environ['DB_ANON_ROLE'])
+    cur.execute('REVOKE INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA ' + os.environ['DB_SCHEMA'] + ' FROM ' + os.environ['DB_ANON_ROLE'])
+
+    # Enable the connecting user to assume the anonymous role
+    cur.execute('GRANT ' + os.environ['DB_ANON_ROLE'] + ' TO ' + os.environ['DB_USERNAME'])
+
+except:
+    print "WARNING: Unable to create role " + os.environ['DB_ANON_ROLE']
+    print "If you're using shared-psql, try upgrading to the medium-psql plan!"
+
+# Run any provided SQL files in alphabetical order
 for sqlfile in sorted(sys.argv[1:]):
     print 'Running ' + sqlfile
     try:
--- start.sh
+++ start.sh
@@ -9,5 +9,8 @@ export PATH=$PATH:/home/vcap/deps/0/python/bin/
 # We only do this when the first instance comes up; no need for every instance to do it.
 [[ $CF_INSTANCE_INDEX -eq 0 && -x ./data/init.sh ]] && ./data/init.sh 
 
+# Set DB_USERNAME, which is used when granting access to the anonymous role
+[[ $DATABASE_URL =~ (.*)'://'(.*):(.*)@(.*):(.*?)/(.*) ]] && export DB_USERNAME=${BASH_REMATCH[2]}
+
 export DB_URI=${DATABASE_URL}
 exec ./env-to-config ./postgrest postgrest.conf
