bdd937aa950e502f6c87e783b29b421302e5e2bc
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-03-06 17:08:27 -0800

    Go back to bringing in the db_anon_role from vars.yml

--- start.sh
+++ start.sh
@@ -9,8 +9,5 @@ export PATH=$PATH:/home/vcap/deps/0/python/bin/
 # We only do this when the first instance comes up; no need for every instance to do it.
 [[ $CF_INSTANCE_INDEX -eq 0 && -x ./data/init.sh ]] && ./data/init.sh 
 
-# For now, let's just use the role that the platform gave us for the anonymous role
-[[ $DATABASE_URL =~ (.*)'://'(.*):(.*)@(.*):(.*?)/(.*) ]] && export DB_ANON_ROLE=${BASH_REMATCH[2]}
-
 export DB_URI=${DATABASE_URL}
 exec ./env-to-config ./postgrest postgrest.conf
