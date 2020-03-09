7143ba3e91836632782f150e81e98b7f9cd0e014
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-03-09 12:35:00 -0700

    Use medium-psql plan, and improve error-handling for the deploy script
    Note the change to medium-psql, which enables setting up roles in the DB!
    This is necessary for now to ensure that the DB doesn't expose all the tables in the public schema
    as writeable. However, this kind of DB can take several minutes to come up! So there's now a loop
    in here to wait for that to complete.
    
    A commit to come will remove the parameterization of the anonymous role so that `data/*.sql` files
    can be explicit when referring to roles to grant/revoke.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-# Get the binary for the latest version of PostgREST
-curl -L `curl https://api.github.com/repos/postgrest/postgrest/releases/latest 2>/dev/null |  jq -r '.assets[] | select(.browser_download_url | contains("ubuntu.tar")) | .browser_download_url' ` | tar xzv
+# Stop processing if anything goes wrong
+set -e
 
-# Get a script that will map env vars to a postgrest.conf file and then run the binary
+# Get the binary for the latest version of PostgREST
+curl -L `curl -s https://api.github.com/repos/postgrest/postgrest/releases/latest 2>/dev/null |  jq -r '.assets[] | select(.browser_download_url | contains("ubuntu.tar")) | .browser_download_url' ` | tar xzv
+
+# Get a convenient script that maps env vars to a postgrest.conf file and then runs the binary
 curl https://raw.githubusercontent.com/PostgREST/postgrest-heroku/master/env-to-config > env-to-config
 
 # Make both files executable
 chmod +x postgrest env-to-config
 
 # Create a database service
-# (NOTE: This is the default name; change it if you edit the vars.yml app_name default!)
-# (NOTE: If you want a DB where you can create explicit roles, don't use the shared plan!)
-cf create-service aws-rds shared-psql postgrest-db
+# (NOTE: postgrest-db is the *default* name; if you edit the app_name in vars.yml change this to match!)
+# (NOTE: If you want a REST API with access control, use the medium-psql plan or better!)
+cf create-service aws-rds medium-psql postgrest-db
+
+# Wait until the DB service is both available and ready for use
+# See https://cloud.gov/docs/services/relational-database/#instance-creation-time
+until cf create-service-key postgrest-db ready-yet > /dev/null 2>&1
+do 
+    echo "Waiting for the database to spin up..."
+    sleep 10
+done
+
+# Delete the canary key since it's no longer needed
+cf delete-service-key -f postgrest-db ready-yet > /dev/null 2>&1
 
 # Push the application up; this will error until you copy vars.yml.template to vars.yml!
-cf push --vars-file vars.yml
+[[ -e vars.yml ]] \
+    && cf push --vars-file vars.yml \
+    || echo "vars.yml not found; copy and optionally customize vars.yml.template!"
 
